09adf118f0e26af02ba35c11b40bec54642d0579
Peter DeMartini <peterdemartini@me.com>
2014-09-02 16:49:08 -0700

    Fixed bugs

--- heartrate.js
+++ heartrate.js
@@ -24,9 +24,23 @@ function getPeripherals(fn) {
 }
 
 function discover(fn) {
+	function onRead(error, data) {
+	  if (data) {
+	    var returnObj = {
+	      heartRate: data.readUInt8(1)
+	    };
+	    console.log('heart rate', returnObj.heartRate);
+	    fn(returnObj);
+	  } else {
+	    fn({
+	      error: 'Unable to read to heart rate'
+	    });
+	  }
+	}
   _.each(peripherals, function(peripheral) {
     peripheral.connect(function() {
       console.log('Connected to peripheral');
+      // Android Specific
       peripheral.discoverServices(serviceUuids, function(error, services) {
         var deviceService = services[0];
         if (!deviceService) {
@@ -47,42 +61,11 @@ function discover(fn) {
             var mainCharacteristic = characteristics[0];
             console.log('discovered heart rate characteristic for read', mainCharacteristic.uuid);
 
-            if(typeof window !== 'undefined' && window.device && window.device.platform === 'Android'){
-            	mainCharacteristic.on('notify', function(error, bytes) {
-	              //Check for data
-	              if (bytes.length === 0) {
-	                api.logIt('Subscription result had zero length data');
-	                return;
-	              }
+            // Works on non-phonegap
+            mainCharacteristic.on('read', onRead);
 
-	              //Get the first byte that contains flags
-	              var flag = bytes[0];
-
-	              //Check if u8 or u16 and get heart rate
-	              var hr;
-	              if ((flag & 0x01) === 1) {
-	                var u16bytes = bytes.buffer.slice(1, 3);
-	                var u16 = new Uint16Array(u16bytes)[0];
-	                hr = u16;
-	              } else {
-	                var u8bytes = bytes.buffer.slice(1, 2);
-	                var u8 = new Uint8Array(u8bytes)[0];
-	                hr = u8;
-	              }
-	              console.log('Heart Rate:: ' + hr);
-	              var returnObj = { heartRate : hr };
-	              fn(returnObj);
-	            });
-            }else{
-            	mainCharacteristic.on('read', function(data) {
-	              if (data) {
-	                var returnObj = {
-	                  heartRate: data.readUInt8(1)
-	                };
-	                console.log('heart rate', returnObj.heartRate);
-	                fn(returnObj);
-	              }
-	            });
+            if(typeof window !== 'undefined' && typeof window.bluetoothle !== 'undefined'){
+  	            mainCharacteristic.read(function(){});
             }
 
             // true to enable notify
@@ -109,7 +92,7 @@ lib.init = function(opts, newApi) {
     discover(function(data) {
       if (!data) data = {};
       if (data.error) {
-        console.log('Error in Heartbeat Plugin' + JSON.stringify(data.error));
+        console.log('Error in Heartbeat Plugin: ' + JSON.stringify(data.error));
         api.logIt(data.error);
       } else {
         api.logHeartrate(data.heartRate);
