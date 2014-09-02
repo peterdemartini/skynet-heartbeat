e132933f6e8699bbad8468f2ff6dfbad6851e284
Peter DeMartini <peterdemartini@me.com>
2014-09-02 08:25:49 -0700

    0.0.1: Inital Build

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-skynet-heartbeat
+Skynet Heartbeat
 ================
 
-Skynet Heartbeat
+Connect Mobiblu and Gateblu to a BLE heart rate monitor like the Polar H7
---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+gulp.task('default', function() {
+    gulp.src('index.js')
+        .pipe(browserify({
+            insertGlobals : true,
+            standalone : 'skynetPlugins.' + require('./package.json').name,
+            debug : true
+        }))
+        .pipe(rename('bundle.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./'))
+});
