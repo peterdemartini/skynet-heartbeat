f97ce2d01d7cfdbc7d5597339aaecb48fce255a6
Peter DeMartini <peterdemartini@me.com>
2014-09-03 10:36:26 -0700

    Better structure & Handle already connected

--- heartrate.js
+++ heartrate.js
@@ -12,7 +12,7 @@ var lib = {},
   characteristicUuid = '2a37';
 
 function getPeripherals(fn) {
-  scan(10 * 1000, serviceUuids, peripherals, function(peripherals) {
+  scan(15 * 1000, serviceUuids, peripherals, function(peripherals) {
     console.log('finished scanning', peripherals);
     peripherals = peripherals.map(function(peripheral) {
       var p = _.clone(peripheral);
@@ -24,66 +24,90 @@ function getPeripherals(fn) {
 }
 
 function discover(fn) {
-	function getHeartrate(data){
-      if(data instanceof Uint8Array){
+  _.each(peripherals, function(peripheral) {
+    function getHeartrate(data) {
+      if (data instanceof Uint8Array) {
         return data;
-      }else{
+      } else {
         return data.readUInt8(1);
       }
-  }
-  function onRead(data) {
-	  if (data) {
-	    var obj = {
-	      heartRate: getHeartrate(data)
-	    };
-	    console.log('heart rate', obj.heartRate);
-	    fn(obj);
-	  } else {
-	    fn({
-	      error: 'Unable to read to heart rate'
-	    });
-	  }
-	}
-  _.each(peripherals, function(peripheral) {
-    peripheral.connect(function() {
-      console.log('Connected to peripheral');
-      // Android Specific
+    }
+
+    function onRead(data) {
+      if (data) {
+        var obj = {
+          heartRate: getHeartrate(data)
+        };
+        console.log('heart rate', obj.heartRate);
+        fn(obj);
+      } else {
+        fn({
+          error: 'Unable to read to heart rate'
+        });
+      }
+    }
+
+    function subsribe(mainCharacteristic) {
+      mainCharacteristic.on('read', onRead);
+
+      // true to enable notify
+      mainCharacteristic.notify(true, function(error) {
+        console.log('Heart rate level notification');
+        if (error) {
+          fn({
+            error: 'Unable to subscribe to heart rate'
+          });
+        }
+      });
+    }
+
+    function getCharacteristics(deviceService) {
+      deviceService.discoverCharacteristics([characteristicUuid], function(error, characteristics) {
+        if (error) {
+          console.log('Error discovering characteristics', error);
+          fn({
+            error: error
+          });
+        } else {
+          var mainCharacteristic = characteristics[0];
+          console.log('Discovered heart rate characteristic for read', mainCharacteristic.uuid);
+          subsribe(mainCharacteristic);
+        }
+      });
+    }
+
+    function getServices() {
       peripheral.discoverServices(serviceUuids, function(error, services) {
         var deviceService = services[0];
         if (!deviceService) {
-          console.log('no heart rate service found', services);
+          var msg = 'No heart rate service found';
+          console.log('Discover services error', services);
           return fn({
-            error: 'No heart rate service found'
+            error: msg
           });
         } else {
-          console.log('discovered heart rate service for read', deviceService.uuid);
+          console.log('Discovered heart rate service for read', deviceService.uuid);
         }
-        deviceService.discoverCharacteristics([characteristicUuid], function(error, characteristics) {
-          if (error) {
-            console.log('error discovering characteristics', error);
-            fn({
-              error: error
-            });
-          } else {
-            var mainCharacteristic = characteristics[0];
-            console.log('discovered heart rate characteristic for read', mainCharacteristic.uuid);
-
-            // Works on non-phonegap
-            mainCharacteristic.on('read', onRead);
-
-            // true to enable notify
-            mainCharacteristic.notify(true, function(error) {
-              console.log('heart rate level notification');
-              if (error) {
-                fn({
-                  error: 'Unable to subscribe to heart rate'
-                });
-              }
-            });
-          }
-        });
+        getCharacteristics(deviceService);
       });
-    });
+    }
+
+    function onConnect() {
+      getServices();
+    }
+
+    function connect() {
+      peripheral.connect(onConnect);
+    }
+
+    if (peripheral.state === 'connected') {
+      console.log('Already connected to peripheral');
+      onConnect();
+    } else {
+      console.log('Connecting to peripheral');
+      connect();
+    }
+
   });
 }
 
