36349e728599a9c07e115882b11f273c2273ce24
Peter DeMartini <peterdemartini@me.com>
2014-10-27 15:26:58 -0700

    Refactored

--- heartrate.js
+++ heartrate.js
@@ -8,146 +8,138 @@ var lib = {},
   api,
   serviceUuid = '180d',
   serviceUuids = [serviceUuid],
-  peripherals = [],
+  peripheral,
   characteristicUuid = '2a37';
 
-function getPeripherals(fn) {
-  scan(25 * 1000, serviceUuids, peripherals, function(peripherals) {
-    console.log('finished scanning', peripherals);
-    peripherals = peripherals.map(function(peripheral) {
-      var p = _.clone(peripheral);
-      delete p._noble;
-      return p;
-    });
+function getPeripheral(fn) {
+  new scan(25 * 1000, serviceUuids, function(_peripheral) {
+    peripheral = _peripheral;
     fn();
   }, api.logIt);
 }
 
 function discover(fn) {
-  _.each(peripherals, function(peripheral) {
-    function getHeartrate(data) {
-      if (data instanceof Uint8Array) {
-        var bytes = data;
-        //Check for data
-        if (bytes.length === 0) {
-          console.log('Subscription result had zero length data');
-          return;
-        }
-
-        //Get the first byte that contains flags
-        var flag = bytes[0];
-
-        //Check if u8 or u16 and get heart rate
-        var hr;
-        if ((flag & 0x01) === 1) {
-          var u16bytes = bytes.buffer.slice(1, 3);
-          var u16 = new Uint16Array(u16bytes)[0];
-          hr = u16;
-        } else {
-          var u8bytes = bytes.buffer.slice(1, 2);
-          var u8 = new Uint8Array(u8bytes)[0];
-          hr = u8;
-        }
-        return hr;
-      } else {
-        return data.readUInt8(1);
+  function getHeartrate(data) {
+    if (data instanceof Uint8Array) {
+      var bytes = data;
+      //Check for data
+      if (bytes.length === 0) {
+        console.log('Subscription result had zero length data');
+        return;
       }
-    }
 
-    function onRead(data) {
-      if (data) {
-        var obj = {
-          heartRate: getHeartrate(data)
-        };
-        console.log('heart rate', obj.heartRate);
-        fn(obj);
+      //Get the first byte that contains flags
+      var flag = bytes[0];
+
+      //Check if u8 or u16 and get heart rate
+      var hr;
+      if ((flag & 0x01) === 1) {
+        var u16bytes = bytes.buffer.slice(1, 3);
+        var u16 = new Uint16Array(u16bytes)[0];
+        hr = u16;
       } else {
+        var u8bytes = bytes.buffer.slice(1, 2);
+        var u8 = new Uint8Array(u8bytes)[0];
+        hr = u8;
+      }
+      return hr;
+    } else {
+      return data.readUInt8(1);
+    }
+  }
+
+  function onRead(data) {
+    if (data) {
+      var obj = {
+        heartRate: getHeartrate(data)
+      };
+      console.log('heart rate', obj.heartRate);
+      fn(obj);
+    } else {
+      fn({
+        error: 'Unable to read to heart rate'
+      });
+    }
+  }
+
+  function subsribe(mainCharacteristic) {
+    mainCharacteristic.on('read', onRead);
+
+    // true to enable notify
+    mainCharacteristic.notify(true, function(error) {
+      console.log('Heart rate level notification');
+      if (error) {
         fn({
-          error: 'Unable to read to heart rate'
+          error: 'Unable to subscribe to heart rate'
         });
       }
-    }
+    });
+  }
 
-    function subsribe(mainCharacteristic) {
-      mainCharacteristic.on('read', onRead);
-
-      // true to enable notify
-      mainCharacteristic.notify(true, function(error) {
-        console.log('Heart rate level notification');
-        if (error) {
-          fn({
-            error: 'Unable to subscribe to heart rate'
-          });
-        }
-      });
-    }
-
-    function getCharacteristics(deviceService) {
-      deviceService.discoverCharacteristics([characteristicUuid], function(error, characteristics) {
-        if (error) {
-          console.log('Error discovering characteristics', error);
-          fn({
-            error: error
-          });
-        } else {
-          var mainCharacteristic = characteristics[0];
-          console.log('Discovered heart rate characteristic for read', mainCharacteristic.uuid);
-          subsribe(mainCharacteristic);
-        }
-      });
-    }
-
-    function getServices() {
-      peripheral.discoverServices(serviceUuids, function(error, services) {
-        var deviceService = services[0];
-        if (!deviceService) {
-          var msg = 'No heart rate service found';
-          console.log('Discover services error', services);
-          return fn({
-            error: msg
-          });
-        } else {
-          console.log('Discovered heart rate service for read', deviceService.uuid);
-        }
-        getCharacteristics(deviceService);
-      });
-    }
-
-    function disonnect() {
-      peripheral.disconnect();
-    }
-
-    function readyForDisonnect() {
-      if (document) {
-        document.addEventListener('pause', disonnect);
+  function getCharacteristics(deviceService) {
+    deviceService.discoverCharacteristics([characteristicUuid], function(error, characteristics) {
+      if (error) {
+        console.log('Error discovering characteristics', error);
+        fn({
+          error: error
+        });
+      } else {
+        var mainCharacteristic = characteristics[0];
+        console.log('Discovered heart rate characteristic for read', mainCharacteristic.uuid);
+        subsribe(mainCharacteristic);
       }
-    }
+    });
+  }
 
-    function onConnect() {
-      readyForDisonnect();
-      getServices();
-    }
+  function getServices() {
+    peripheral.discoverServices(serviceUuids, function(error, services) {
+      var deviceService = services[0];
+      if (!deviceService) {
+        var msg = 'No heart rate service found';
+        console.log('Discover services error', services);
+        return fn({
+          error: msg
+        });
+      } else {
+        console.log('Discovered heart rate service for read', deviceService.uuid);
+      }
+      getCharacteristics(deviceService);
+    });
+  }
 
-    function connect() {
-      peripheral.connect(onConnect);
-    }
+  function disonnect() {
+    peripheral.disconnect();
+  }
 
-    if (peripheral.state === 'connected') {
-      console.log('Already connected to peripheral');
-      onConnect();
-    } else {
-      console.log('Connecting to peripheral');
-      connect();
+  function readyForDisonnect() {
+    if (typeof document !== 'undefined') {
+      document.addEventListener('pause', disonnect);
     }
+  }
 
-  });
+  function onConnect() {
+    readyForDisonnect();
+    getServices();
+  }
+
+  function connect() {
+    peripheral.connect(onConnect);
+  }
+
+  if (peripheral.state === 'connected') {
+    console.log('Already connected to peripheral');
+    onConnect();
+  } else {
+    console.log('Connecting to peripheral');
+    connect();
+  }
 }
 
 lib.init = function(opts, newApi) {
   options = opts;
   api = newApi;
 
-  getPeripherals(function() {
+  getPeripheral(function() {
     discover(function(data) {
       if (!data) data = {};
       if (data.error) {
--- scan.js
+++ scan.js
@@ -1,44 +1,49 @@
 'use strict';
 
 var noble = require('noble');
-var stopandreturn, discover;
 
-stopandreturn = function() {
-  noble.stopScanning();
-  noble.removeListener('discover', discover);
-  console.log('Stop Scanning for BLE devices...');
-  this.logIt(null, 'Stopped Scanning for Heartbeat Devices');
-  this.done(this.peripherals);
-};
+function Scanner(timeout, serviceUuids, done, logIt) {
+	var self = this;
 
-discover = function(peripheral) {
-  console.log('(scan)found: ' + peripheral.advertisement.localName);
-  this.logIt(null, 'Found Heartbeat Device : ' + peripheral.advertisement.localName);
-  if (peripheral) {
-    this.peripherals.unshift(peripheral);
-    stopandreturn.bind(this);
-  } else {
-    this.logIt('Invalid Peripheral');
-    console.log('Invalid Peripheral');
-  }
-};
+	self.logIt = logIt;
 
-module.exports = function(timeout, serviceUuids, peripherals, done, logIt) {
-  noble.on('discover', discover.bind({
-    peripherals: peripherals,
-    logIt: logIt,
-    timeout: timeout
-  }));
+	self.peripheral = null;
+
+	self.done = done;
+
+  noble.on('discover', self.discoverMonitor.bind(self));
   if (!Array.isArray(serviceUuids)) {
     serviceUuids = [serviceUuids];
   }
   noble.startScanning(serviceUuids);
-  console.log('Scanning for BLE devices...');
-  logIt(null, 'Scanning for Heartbeat Devices');
-  setTimeout(stopandreturn.bind({
-    timeout: timeout,
-    peripherals: peripherals,
-    done: done,
-    logIt: logIt
-  }), timeout);
+
+  self.logEvent('Scanning for Heartbeat Devices');
+  setTimeout(self.stopScanning, timeout);
+}
+
+Scanner.prototype.logEvent = function (err, msg){
+	console.log(err || msg);
+ 	this.logIt(err, msg);
 };
+
+Scanner.prototype.stopScanning = function() {
+	var self = this;
+  noble.stopScanning();
+  noble.removeListener('discover', self.discoverMonitor);
+  self.logEvent('Stop Scanning for BLE devices...');
+  self.done(self.peripheral);
+};
+
+Scanner.prototype.discoverMonitor = function (peripheral) {
+	var self = this;
+  self.logEvent('Found Heartbeat Device : ' + peripheral.advertisement.localName);
+  if (peripheral) {
+  	self.peripheral = peripheral;
+    self.stopScanning();
+  } else {
+    self.logIt('Invalid Peripheral');
+    console.log('Invalid Peripheral');
+  }
+};
+
+module.exports = Scanner;
